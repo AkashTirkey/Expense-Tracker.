1e830dc1d931f370d3824d63b6c0b827104646a8
Akash Tirkey <akash.tirkeywork@gmail.com>
2025-02-06 15:41:56 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const expenseForm = document.getElementById("expenseForm");
+const expenseList = document.getElementById("expenselist");
+
+expenseForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const description = document.getElementById("description").value.trim();
+  const category = document.getElementById("category").value;
+  const amount = parseFloat(document.getElementById("amount").value);
+
+  if (
+    description &&
+    category !== "Select Category" &&
+    !isNaN(amount) &&
+    amount > 0
+  ) {
+    const newRow = document.createElement("tr");
+
+    newRow.innerHTML = `<td>${description}</td>
+                        <td>${category}</td>
+                        <td>${amount}</td>`;
+
+    expenseList.appendChild(newRow);
+
+    // Reset Form Fields
+    document.getElementById("description").value = "";
+    document.getElementById("category").value = "Select Category";
+    document.getElementById("amount").value = "";
+  } else {
+    alert("Please fill out all fields with valid data.");
+  }
+});
